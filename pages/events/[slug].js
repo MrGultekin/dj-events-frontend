76338b3fc081e538e147735833aa2a8a355e0dfc
MrGultekin <gultekin.de@gmail.com>
2022-09-14 22:25:30 +0200

    router.push('/') tried

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,12 @@
+import {useRouter} from "next/router";
+
 export default function EventPage() {
+	const router = useRouter();
+	console.log(router)
 	return (
 		<div>
 			<h1>My Single Event</h1>
+			<button onClick={()=>router.push('/')}>Click to go to HOME</button>
 		</div>
 	);
 }
