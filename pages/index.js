0c84baf8cf91bc4f2e808b2ec53a7c2d7d8aba67
MrGultekin <gultekin.de@gmail.com>
2022-09-14 22:15:22 +0200

    cleaned up <Link>

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from "next/link";
 
 export default function HomePage() {
   return (
       <div>
         <h1>Home</h1>
-          <Link href="/about">go to about</Link>
       </div>
   )
 }
