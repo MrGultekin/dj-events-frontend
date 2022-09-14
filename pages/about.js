9d31aa1aab5e7a152f6b1e20030766341cf06ca7
MrGultekin <gultekin.de@gmail.com>
2022-09-14 21:38:41 +0200

    Unversioned Files ? :-)

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
-import React from 'react';
 
-function About(props) {
+export default function AboutPage() {
 	return (
-		<div></div>
+		<div>
+			<h1>About Page</h1>
+		</div>
 	);
 }
 
-export default About;
