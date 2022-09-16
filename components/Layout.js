6a2d1e00a8284ba8bf8c13c195adaae65cfc54d1
MrGultekin <gultekin.de@gmail.com>
2022-09-16 07:35:45 +0200

    css added

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from '../styles/Layout.module.css'
 
 export default function Layout({title, keywords, description, children}) {
 	return (
@@ -7,13 +8,16 @@ export default function Layout({title, keywords, description, children}) {
 				<title>{title}</title>
 				<meta name='description' content={description}/>
 				<meta name={'keywords'} content={keywords}/>
-				{children}
 			</Head>
+			
+			<div className={styles.container}>
+				{children}
+			</div>
 		</div>
 	);
 }
-Layout.defaultProps={
+Layout.defaultProps = {
 	title: 'DJ Events | Find the hottest parties',
 	description: 'Find the latest DJ and other musical events',
-	keywords:'music, dj, edm, events'
+	keywords: 'music, dj, edm, events'
 }
